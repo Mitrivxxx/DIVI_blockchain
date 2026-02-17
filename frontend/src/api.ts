70e15e12ceeed4a8ee12af342a2b86248207f1f3
matsma <matsma2468@gmail.com>
2026-02-17 10:48:50 +0100

    frontend->blockchain

--- frontend/src/api.ts
+++ frontend/src/api.ts
@@ -1,3 +1,1 @@
-// Central API URL for backend
-// Change this if backend URL changes (e.g. for production)
-export const API_URL = "http://localhost:5021"; // default .NET port, adjust if needed
+export const API_URL = "http://localhost:5021";
